e74041768a7104348358051518ba8ebc8c5901a1
Rizwan Walayat <rizwan.wallayat@gmail.com>
2025-02-11 21:42:23 +0500

    Webview added to Form View Controller

--- Forms Gen/Controllers/Forms/FormViewController.swift
+++ Forms Gen/Controllers/Forms/FormViewController.swift
@@ -8,22 +8,26 @@
 
 import UIKit
 import WebKit
+import SafariServices
 
 class FormViewController: UIViewController {
 
-    var webView: WKWebView!
+    @IBOutlet weak var webView: WKWebView!
+//    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Initialize and configure WKWebView
-        webView = WKWebView(frame: self.view.frame)
-        self.view.addSubview(webView)
-
+        let url = "https://docs.google.com/forms/d/1Sc9hxfMrAXsFbYPVXzYBi52I9QJR76fwRxnl5169Muo/edit"
+//        let url = "https://google.com"
         // Replace this with your Google Form URL
-        if let googleFormURL = URL(string: "https://docs.google.com/forms/d/1Sc9hxfMrAXsFbYPVXzYBi52I9QJR76fwRxnl5169Muo/edit") {
+        if let googleFormURL = URL(string: url) {
             let request = URLRequest(url: googleFormURL)
+//            request.addValue("Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/537.36", forHTTPHeaderField: "User-Agent")
+
             webView.load(request)
+            webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/537.36"
+
         }
     }
 
